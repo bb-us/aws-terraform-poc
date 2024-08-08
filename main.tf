7d8a602b885dd816173a688dcb755540dd026ac0
bb-us <brandon.bennett.us@gmail.com>
2024-08-07 21:54:29 -0400

    Removing asg and alb module main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source  = "git::https://github.com/Coalfire-CF/terraform-aws-vpc.git"
+  source  = "git::https://github.com/Coalfire-CF/terraform-aws-vpc-nfw.git"
   name    = var.vpc_name
   cidr    = var.vpc_cidr
   azs     = var.availability_zones
@@ -12,7 +12,7 @@ module "vpc" {
 }
 
 module "ec2_instance" {
-  source            = "git::https://github.com/Coalfire-CF/terraform-aws-ec2-instance.git"
+  source            = "git::https://github.com/Coalfire-CF/terraform-aws-ec2.git"
   instance_type     = var.ec2_instance_type
   ami               = var.ec2_ami
   subnet_id         = module.vpc.public_subnets[1]
@@ -23,43 +23,4 @@ module "ec2_instance" {
   tags = {
     Name = "poc-ec2-instance"
   }
-}
-
-module "autoscaling" {
-  source             = "git::https://github.com/Coalfire-CF/terraform-aws-autoscaling.git"
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.private_subnets
-  instance_type      = var.asg_instance_type
-  min_size           = var.asg_min_size
-  max_size           = var.asg_max_size
-  desired_capacity   = var.asg_desired_capacity
-  health_check_type  = "EC2"
-  launch_template_id = aws_launch_template.app_launch_template.id
-  tags = {
-    Name = "poc-asg-instance"
-  }
-}
-
-module "alb" {
-  source  = "git::https://github.com/Coalfire-CF/terraform-aws-alb.git"
-  name    = var.alb_name
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.public_subnets
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      default_action_type = "forward"
-    }
-  ]
-
-  target_groups = [
-    {
-      name     = "poc-target-group"
-      port     = 443
-      protocol = "HTTP"
-      vpc_id   = module.vpc.vpc_id
-    }
-  ]
 }
